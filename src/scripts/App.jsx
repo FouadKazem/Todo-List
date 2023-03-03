1992991520fff490d3e2d8ec5f3cb229d9be89be
Fouad Kazem <kazemfouad53@gmail.com>
2023-03-03 10:26:52 +0400

    Modified App.jsx, Header.jsx, Main.jsx, and Task.jsx
    App.jsx:
    - Modified 'addField' event handler to make it asking for new field's title, and handle the cancellation of adding the new field when the user presses the cancel button or provides empty title.
    - Added 'switchTheme' event handler which handles the changing of website theme, and drill the event handler to Header component.
    - Added 'editFieldTitle' event handler which handles the changing of field title by knowing the field id, and drill the event handler to Header component.
    - Added 'addTask' event handler which handles adding new tasks to a field by knowing the field id, and drill the event handler to Main component.
    - Added 'checkTask' event handler which handles changing the status of a task from 'undone' to 'done' and vice versa by knowing the field id and task id, and drill the event handler to Main component.
    - Added 'editTaskDescription' event handler which handles editing task description by knowing the field id and task id, and drill the event handler to Main component.
    - Added 'deleteTask' event handler which handles deleting task by knowing the field id and task id, and drill the event handler to Main component.
    - Added 'windowHeight' state, 'headerHeight' state, and 'mainStyles' state to assign static height to the Main component and prevent it from expanding, and drill the 'mainStyles' state to Main component.
    - Added two side effects handlers. The first one assigns event listener of type 'resize' to the window object in order to assign the value of window height to 'windowHeight' state, and adds observer to watch the changing of Header height in order to assign the value of header height to 'headerHeight' state. The second one runs when a change happens to the states 'windowHeight' and 'headerHeight', and assign new object (contains the new calculated height) to the 'mainStyles' state.
    Header.jsx:
    - Added 'scrollFieldTabs' event handler which handles horizontal scrolling of fields tabs.
    - Added 'startClientX' state to watch the horizontal location of touch event on field tabs area.
    - Added 'startTouchFieldTabs' and 'moveTouchFieldTabs' event handlers which handle horizontal scrolling of fields tabs in touch screens.
    Main.jsx:
    - Added 'scrollField' event handler which handles vertical scrolling of field tasks.
    - Added 'startClientY' state to watch the vertical location of touch event on field area.
    - Added 'startTouchField' and 'moveTouchFieldTabs' event handlers which handle vertical scrolling of field tasks in touch screens.
    - Added new message that gives the ability to add task if field is empty.
    - Added new button in the end of Main component which appears when the field is not empty and gives the ability to add new task.
    Task.jsx:
    - Added a section for task description and three buttons for checking, modifying, and deleting tasks.

--- src/scripts/App.jsx
+++ src/scripts/App.jsx
@@ -8,27 +8,91 @@ function App() {
     // If there are no such data in the local storage it will assign the
     // object next to the or operator.
     const [fields, setFields] = React.useState(() => {
-        return JSON.parse(localStorage.getItem('fields')) || { fieldsArray: [], activeFieldId: null }
+        return JSON.parse(localStorage.getItem('fields')) || { fieldsArray: [], activeFieldId: null, theme: 'light' }
     })
+    const [windowHeight, setWindowHeight] = React.useState(0)
+    const [headerHeight, setHeaderHeight] = React.useState(0)
+    const [mainStyles, setMainStyles] = React.useState({})
 
     // Side effect will only run if the fields object changed, it saves the
     // current fields data to the local storage.
     React.useEffect(() => localStorage.setItem('fields', JSON.stringify(fields)), [fields])
 
+    // Side Effect to listen to change of the size of the window and the header.
+    React.useEffect(() => {
+        window.addEventListener('resize', () => {
+            setWindowHeight(window.innerHeight)
+        })
+        new ResizeObserver(() => {
+            setHeaderHeight(document.querySelector('header').clientHeight)
+        }).observe(document.querySelector('header'))
+    }, [])
+
+    // Side effect to modify the main styles based on changing in the height of
+    // window and header heights states.
+    React.useEffect(() => {
+        setMainStyles({
+            height: `${windowHeight - headerHeight}px`
+        })
+    }, [windowHeight, headerHeight])
+
+    // Event handler will change the theme from light to dark and vice versa.
+    function switchTheme() {
+        setFields(prevFields => {
+            return {
+                ...prevFields,
+                theme: prevFields.theme == 'light' ? 'dark' : 'light'
+            }
+        })
+    }
+
     // Event handler will create new field and add it to the end of array.
     function addField() {
+        const newFieldTitle = prompt('Enter Title for the New Field')
+        if (!newFieldTitle) {
+            if (newFieldTitle == '') {
+                alert('Field Title Cannot be Empty!')
+            }
+            return
+        }
+        const newFieldId = nanoid()
         setFields(prevFields => {
-            const newFieldId = nanoid()
             return {
+                ...prevFields,
                 activeFieldId: prevFields.activeFieldId == null ? newFieldId : prevFields.activeFieldId,
                 fieldsArray: [
                     ...prevFields.fieldsArray,
                     {
                         fieldId: newFieldId,
-                        fieldTitle: `Field ${prevFields.fieldsArray.length + 1}`,
+                        fieldTitle: newFieldTitle,
                         fieldTasks: []
                     }
                 ]
+            }
+        })
+    }
+
+    // Event handler will edit the field title based on the id.
+    function editFieldTitle(id) {
+        const newFieldTitle = prompt('Enter New Title for Field')
+        if (!newFieldTitle) {
+            if (newFieldTitle == '') {
+                alert('Field Title Cannot be Empty!')
+            }
+            return
+        }
+        setFields(preFields => {
+            return {
+                ...preFields,
+                fieldsArray: preFields.fieldsArray.map(field => {
+                    if (field.fieldId == id) {
+                        return {
+                            ...field,
+                            fieldTitle: newFieldTitle
+                        }
+                    }
+                    return field
+                })
             }
         })
     }
@@ -78,10 +142,129 @@ function App() {
         })
     }
 
+    // Event handler will add task to a field based on the passed field id.
+    function addTask(id) {
+        const newTaskDescription = prompt('Enter Task Description')
+        if (!newTaskDescription) {
+            if (newTaskDescription == '') {
+                alert('Task Description Cannot be Empty!')
+            }
+            return
+        }
+        setFields(prevFields => {
+            return {
+                ...prevFields,
+                fieldsArray: prevFields.fieldsArray.map(field => {
+                    if (field.fieldId == id) {
+                        return {
+                            ...field,
+                            fieldTasks: [
+                                ...field.fieldTasks,
+                                {
+                                    taskId: nanoid(),
+                                    taskDescription: newTaskDescription,
+                                    taskStatus: false
+                                }
+                            ]
+                        }
+                    }
+                    return field
+                })
+            }
+        })
+    }
+
+    // Event handler will switch a task from done to undone and vic versa,
+    function checkTask(fieldId, taskId) {
+        setFields(prevFields => {
+            return {
+                ...prevFields,
+                fieldsArray: prevFields.fieldsArray.map(field => {
+                    if (field.fieldId != fieldId) {
+                        return field
+                    }
+                    return {
+                        ...field,
+                        fieldTasks: field.fieldTasks.map(task => {
+                            return task.taskId == taskId ? { ...task, taskStatus: !task.taskStatus } : task
+                        })
+                    }
+                })
+            }
+        })
+    }
+
+    // Event handler will edit task description.
+    function editTaskDescription(fieldId, taskId) {
+        const newTaskDescription = prompt('Enter New Description for The Task')
+        if (!newTaskDescription) {
+            if (newTaskDescription == '') {
+                alert('Task Description Cannot be Empty!')
+            }
+            return
+        }
+        setFields(prevFields => {
+            return {
+                ...prevFields,
+                fieldsArray: prevFields.fieldsArray.map(field => {
+                    if (field.fieldId != fieldId) {
+                        return field
+                    }
+                    return {
+                        ...field,
+                        fieldTasks: field.fieldTasks.map(task => {
+                            if (task.taskId == taskId) {
+                                return {
+                                    ...task,
+                                    taskDescription: newTaskDescription
+                                }
+                            }
+                            return task
+                        })
+                    }
+                })
+            }
+        })
+    }
+
+    // Event handler will delete task based on field id and task id.
+    function deleteTask(fieldId, taskId) {
+        setFields(prevFields => {
+            return {
+                ...prevFields,
+                fieldsArray: prevFields.fieldsArray.map(field => {
+                    if (field.fieldId != fieldId) {
+                        return field
+                    }
+                    const newFieldTasks = [...field.fieldTasks].filter(task => task.taskId != taskId)
+                    return {
+                        ...field,
+                        fieldTasks: newFieldTasks
+                    }
+                })
+            }
+        })
+    }
+
     return (
         <React.Fragment>
-            <Header fields={fields} addField={addField} deleteField={deleteField} switchField={switchField} />
-            <Main fields={fields} addField={addField} />
+            <Header
+                fields={fields}
+                switchTheme={switchTheme}
+                addField={addField}
+                deleteField={deleteField}
+                editFieldTitle={editFieldTitle}
+                switchField={switchField}
+            />
+            <Main
+                fields={fields}
+                addField={addField}
+                addTask={addTask}
+                checkTask={checkTask}
+                editTaskDescription={editTaskDescription}
+                deleteTask={deleteTask}
+                mainStyles={mainStyles}
+            />
         </React.Fragment>
     )
 }
--- src/scripts/components/Main.jsx
+++ src/scripts/components/Main.jsx
@@ -1,18 +1,62 @@
 import React from 'react'
+import Task from './Task'
 
 function Main(props) {
+    const [startClientY, setStartClientY] = React.useState(0)
+
+    function startTouchField(event) {
+        setStartClientY(event.touches[0].clientY)
+    }
+
+    function moveTouchField(event) {
+        document.querySelector('.field').scrollBy({
+            top: startClientY < event.touches[0].clientY ? -10 : 10,
+        })
+    }
+
+    function scrollField(event) {
+        document.querySelector('.field').scrollBy({
+            top: event.deltaY < 0 ? -30 : 30,
+        })
+    }
+
+    let TasksElements = null
+    for (let i = 0; i < props.fields.fieldsArray.length; i++) {
+        let field = props.fields.fieldsArray[i]
+        if (field.fieldId == props.fields.activeFieldId) {
+            TasksElements = field.fieldTasks.map(task => <Task activeFieldId={props.fields.activeFieldId} task={task} checkTask={props.checkTask} editTaskDescription={props.editTaskDescription} deleteTask={props.deleteTask} />)
+            break
+        }
+    }
+
     return (
-        <main>
+        <main style={props.mainStyles}>
             {
                 props.fields.activeFieldId == null ?
-                <React.Fragment>
-                    <h1>Press the button below to add new tasks field</h1>
-                    <button onClick={props.addField} className='add-btn' id='main-add-btn'>ADD NEW FIELD</button>
-                </React.Fragment>
-                :
-                <React.Fragment>
-                    
-                </React.Fragment>
+                    <div className='intro'>
+                        <h1>Board is Empty! Press The Button Below to Add New Field</h1>
+                        <button onClick={props.addField} className='btn' id='main-add-field-btn'>ADD NEW FIELD</button>
+                    </div>
+                    :
+                    <React.Fragment>
+                        <div onWheel={(event) => scrollField(event)} onTouchStart={event => startTouchField(event)} onTouchMove={event => moveTouchField(event)} className='field'>
+                            {
+                                TasksElements.length == 0 ?
+                                    <div className='intro'>
+                                        <h1>Field is Empty! Press The Button Below to Add New Task</h1>
+                                        <button onClick={() => props.addTask(props.fields.activeFieldId)} className='btn' id='main-add-task-btn'>ADD NEW TASK</button>
+                                    </div>
+                                    :
+                                    <React.Fragment>
+                                        {TasksElements}
+                                    </React.Fragment>
+                            }
+                        </div>
+                        {
+                            TasksElements.length != 0 &&
+                            <button onClick={() => props.addTask(props.fields.activeFieldId)} className='btn' id='main-add-task-btn'>ADD NEW TASK</button>
+                        }
+                    </React.Fragment>
             }
         </main>
     )
